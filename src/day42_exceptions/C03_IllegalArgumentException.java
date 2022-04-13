a654e0cac01d6d6b2e170f1e908d339c1a8576bc
Mehmet Nail Kocagoz <mehmetnailkocagoz@gmail.com>
2022-04-13 21:50:40 +0200

    13/04

--- src/day42_exceptions/C03_IllegalArgumentException.java
+++ src/day42_exceptions/C03_IllegalArgumentException.java
@@ -1,23 +1,23 @@
 package day42_exceptions;
 import java.util.Scanner;
 public class C03_IllegalArgumentException {
-    /*
-    kullanicidan yasini isteyin
-    kullanici yas olarak negatif bir sayi
-    0 veya
-    120' den buyuk bir sayi girerse illegalargumentexception olusacak sekilde
-    bir program yaziniz
-     */
     public static void main(String[] args)  {
-        Scanner scan=new Scanner(System.in);
-        System.out.println("lutfen yasinizi giriniz");
+        /*
+        Kullanicidan yasini isteyin
+        kullanici yas olarak negatif bir sayi,
+        0 veya
+        120'den buyuk bir sayi girerse illegalArgumentException olusacak sekilde
+        bir program yaziniz
+         */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen Yasinizi giriniz");
         int yas=scan.nextInt();
-        if (yas<=0 || yas>120){
-            //System.out.println("Lutfen gecerli bir yas giriniz");
-            //java bizim istedigimiz durumlarda exception firlatabilir
+        if(yas<=0  || yas>120){
+            // System.out.println("Lutfen gecerli bir yas giriniz");
+            // Java bizim istedigimiz durumlarda exception firlatabilir
             throw new IllegalArgumentException();
-        }else{
-            System.out.println("uygun yas girdiniz ,tesekkurler");
+        } else{
+            System.out.println("Uygun yas girdiniz, tesekkurler");
         }
     }
 }
